d72972b8183d9a336a1be0195c4b5bfbb0b71a38
Sayand Bhattacharya <sayand97@gmail.com>
2017-10-17 11:44:12 +0530

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,32 @@
+import keras
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.utils import np_utils
+
+
+train = pd.read_csv('C:/Users/sayan/Desktop/mnist.csv')
+test = pd.read_csv('C:/Users/sayan/Desktop/test_mnist.csv')
+
+#One-Hot encoding!
+trainY = np_utils.to_categorical(train.values[:,0].astype('int32'), 10)
+testY = np_utils.to_categorical(train.values[:,0].astype('int32'),10)
+
+trainX = train.values[:,1:].astype('float32')
+trainX /= 255
+testX = test.values[:,1:].astype('float32')
+testX/= 255
+
+model = Sequential()
+model.add(Dense(32, input_dim=(28,28)))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(Dense(32))
+model.add(Activation('tanh'))
+model.add(Dropout(0.15))
+model.add(Activation('softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(trainX, trainY, batch_size=32, nb_epoch=1000, validation_data=(testX, testY), shuffle=True, show_accuracy=True)
+result = model.predict_classes(x_test, batch_size=32, verbose=1)
+print(result)
